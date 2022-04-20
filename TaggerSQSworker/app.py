1d3bc7a93b9a023cab143571fa9e669e64c056b4
Mahesh Ramamurthy <ramammah@amazon.com>
2022-04-20 12:37:10 -0500

    new Code

--- TaggerSQSworker/app.py
+++ TaggerSQSworker/app.py
@@ -1,10 +1,18 @@
 import json
+from operator import truediv
 import os
 import boto3
+from botocore.config import Config
 import sys,ast
 from ldap3 import Server, Connection, NTLM, ALL
 import logging
-
+from botoful import Query
+config = Config(
+   retries = {
+      'max_attempts': 20,
+      'mode': 'adaptive'
+   }
+)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 region = os.environ['stackregion']
@@ -13,9 +21,13 @@ basedn = os.environ['basedn']
 ldapdomain = os.environ['domain']
 ldapname = os.environ['user']
 secretname = os.environ['secretname']
+ddbtable= os.environ['ddbtable']
 ldapuser = ldapdomain +'\\'+ldapname
-ssmclient = boto3.client('ssm', region)
-sec = boto3.client(service_name='secretsmanager',region_name=region)
+ssmclient = boto3.client('ssm', region,config=config)
+sec = boto3.client('secretsmanager',region,config=config)
+dynamodb = boto3.resource('dynamodb', region,config=config)
+table = dynamodb.Table(ddbtable)
+querytable = Query(table=ddbtable)
 
 def getadgroups(miid,username):
     logger.info('getting ad group for user %s', username)
@@ -33,10 +45,8 @@ def getadgroups(miid,username):
         response = json.loads(conn.response_to_json())
         logger.info(response)
         return response
-    
-                
-                
-def addtagfunc(ssmclient, miid, tags_list):
+                               
+def addtagfunc(miid, tags_list):
     try:
         wstag = ssmclient.add_tags_to_resource(ResourceType='ManagedInstance',
                     ResourceId= miid,
@@ -46,8 +56,7 @@ def addtagfunc(ssmclient, miid, tags_list):
     except:
        logger.error('failed adding tag') 
 
-
-def removeADgrouptags(ssmclient,miid):
+def removeADgrouptags(miid):
     removekeyarray=[]
     listtag = ssmclient.list_tags_for_resource(
     ResourceType='ManagedInstance',
@@ -56,7 +65,9 @@ def removeADgrouptags(ssmclient,miid):
     for tags in range(len(listtag['TagList'])):
         if listtag['TagList'][tags]['Key'].startswith('ADGroup'):
             removekeyarray.append(listtag['TagList'][tags]['Key']) 
-        else:
+        elif listtag['TagList'][tags]['Key'] == 'directoryid':
+            directoryid=listtag['TagList'][tags]['value']
+        else:    
             continue
     logger.info('got the tags to remove as %s ',removekeyarray)
     if len(removekeyarray) != 0:
@@ -66,7 +77,52 @@ def removeADgrouptags(ssmclient,miid):
                 TagKeys=removekeyarray
                 )
             logger.info('Removed tags %s',removekeyaction)
+    return directoryid
 
+def searchiteminddb(username,directoryid):
+    result = table.key(Username= username, DirectoryID= directoryid).execute(dynamodb)
+    if result.items:
+        return True
+    else:
+        return False
+    
+def addgrouptagtoddb(username,directoryid,tags_list):
+    logger.info ('adding group tag to dynamotable')
+    items = {
+        'Username': username,
+        'DirectoryID': directoryid,
+        'ADGroupTags':tags_list
+            }
+    table.put_item(Item=items)
+
+def addbasetagtoddb(miid,username,directoryid):
+    logger.info('Adding Base Tags')
+    basetagkeylist=[]
+    basetagvaluelist =[]
+    listtag = ssmclient.list_tags_for_resource(
+    ResourceType='ManagedInstance',
+    ResourceId = miid)
+    if len(listtag['TagList']) !=0 :
+        for tags in range(len(listtag['TagList'])):
+            basetagkeylist.append(listtag['TagList'][tags]['Key'])
+            basetagvaluelist.append(listtag['TagList'][tags]['Key'])
+        basetaglist=dict(zip(basetagkeylist,basetagvaluelist))
+        table.update_item(
+        Key={
+             'Username': username,
+            'DirectoryID': directoryid,
+            },
+        UpdateExpression="set BaseTag = :g",
+        ExpressionAttributeValues={
+                ':g': basetaglist
+            },
+        ReturnValues="UPDATED_NEW"
+        )
+        
+            
+    
+    
+    
 def lambda_handler(event, context):
     logger.info('starting worker lambda function')
 
@@ -80,7 +136,7 @@ def lambda_handler(event, context):
             logger.info('instance is %s', mangdinst)
             groupoutput=getadgroups(mangdinst,username)['entries'][0]['attributes']['memberOf']
             if groupoutput:
-                removeADgrouptags(ssmclient,mangdinst)
+                directoryid=removeADgrouptags(mangdinst)
                 tags_list = []
                 for item in range(len(groupoutput)):
                     values = {}
@@ -91,7 +147,17 @@ def lambda_handler(event, context):
                     values['Value'] = "ADGROUP"
                     tags_list.append(values)
                     logger.info('list opg group tags to be added is  %s',tags_list)
-                addtagfunc(ssmclient, mangdinst,tags_list)
+                addgrouptagresut=addtagfunc( mangdinst,tags_list)
+                if addgrouptagresut == 'added_Tag':
+                    if searchiteminddb(username,directoryid):
+                        logger.info('Instance found in Dynamotable')
+                        addgrouptagtoddb(username,directoryid,tags_list)
+                    else:
+                        logger.info('Instance not found in dynamotable')
+                        addbasetagtoddb(username,directoryid,tags_list)
+                        addgrouptagtoddb(username,directoryid,tags_list)
+                        
+                    
    
            
            
